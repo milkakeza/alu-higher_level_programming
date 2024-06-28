0db8386814b2a061db5206af4c30ee732743a6e7
milkakeza <m.isingizwe1@alustudent.com>
2024-06-28 07:45:50 -0700

    FizzBuzz

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,7 +2,7 @@
 
 
 def fizzbuzz():
-    # This is gonna print nbrs from 1 to 100 but when a number is a multiple of 3, it prints fizz; when a nbr is a multiple of 5, it prints buzz; finally when it is a multiple of both, it prints fizzbuzz
+    
     for nbr in range(1, 101):
         if nbr % 3 == 0 and nbr % 5 == 0:
             print("FizzBuzz ", end="")

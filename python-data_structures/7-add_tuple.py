7eb78add3d16ec407b33dc884314c28fc97e8d56
milkakeza <m.isingizwe1@alustudent.com>
2024-06-29 11:43:42 -0700

    Tuples addition

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     newtuple = ()
-    1st_tuple = tuple_a + (0, 0)
-    2ndtuple = tuple_b + (0, 0)
-    newtuple = 1st_tuple[0] + 2ndtuple[0], 1st_tuple[1] + 2ndtuple[1]
+    fst_tuple = tuple_a + (0, 0)
+    scndtuple = tuple_b + (0, 0)
+    newtuple = fst_tuple[0] + scndtuple[0], fst_tuple[1] + scndtuple[1]
     return newtuple

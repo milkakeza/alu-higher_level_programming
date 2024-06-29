463a63d1ffa0fcb03c5669dd4998301795a66bcc
milkakeza <m.isingizwe1@alustudent.com>
2024-06-28 19:01:42 -0700

    Import erthin'

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -5,6 +5,6 @@ if __name__ == "__main__":
     import hidden_4
 
     n = dir(hidden_4)
-    for n in names:
-        if n[:2] != "__":
-            print(n)
+    for name in n:
+        if name[:2] != "__":
+            print(name)

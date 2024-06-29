47e313b052e3ceccc7eeb7dd604c117a08cfa297
milkakeza <m.isingizwe1@alustudent.com>
2024-06-29 11:38:56 -0700

    Copy

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    Thi_copy = my_list.Thi_copy()
+    copy = my_list.copy()
     if idx < 0 or idx > len(my_list) - 1:
-        return Thi_copy
+        return copy
     else:
-        Thi_copy[idx] = element
-        return Thi_copy
+        copy[idx] = element
+        return copy

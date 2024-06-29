f59b6a54014b6f2727dd287e0bdbfe530a2f981f
milkakeza <m.isingizwe1@alustudent.com>
2024-06-29 11:03:02 -0700

    Best score

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,12 +2,13 @@
 
 
 def best_score(a_dictionary):
+
     if a_dictionary:
-        MaList = list(a_dictionary.keys())
-        best_score = 0
-        key = ""
-        for i in Malist:
-            if a_dictionary[i] > best_score:
-                best_score = a_dictionary[i]
-                key = i
-        return key
+        my_list = list(a_dictionary.keys())
+        score = 0
+        leader = ""
+        for i in my_list:
+            if a_dictionary[i] > score:
+                score = a_dictionary[i]
+                leader = i
+        return leader

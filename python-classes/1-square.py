d82c0ebb9448ec663bf3e849bb4da5e8a7b0a6d5
milkakeza <m.isingizwe1@alustudent.com>
2024-07-13 12:09:32 -0700

    Square with size

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 
+"""Define a class Square."""
 
 class Square:
 
+    """Represent a square."""
+
     def __init__(self, size):
+
+         """Initialize a new Square."""
 
         self.__size = size

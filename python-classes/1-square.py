8742d4967a4868b43bcc33bf83fb11b70c83b89e
milkakeza <m.isingizwe1@alustudent.com>
2024-07-13 12:10:31 -0700

    Square with size

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -2,12 +2,14 @@
 
 """Define a class Square."""
 
-class Square:
 
+class Square:
     """Represent a square."""
 
     def __init__(self, size):
+        """Initialize a new Square.
 
-         """Initialize a new Square."""
-
+        Args:
+            size (int): The size of the new square.
+        """
         self.__size = size

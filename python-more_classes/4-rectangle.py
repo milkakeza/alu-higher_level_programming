599de118cb931aec950060c3ccccf70a4d5f28e2
milkakeza <m.isingizwe1@alustudent.com>
2024-07-13 12:34:44 -0700

    More classes Yayyy!

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -1,15 +1,23 @@
 #!/usr/bin/python3
+"""Defines a Rectangle class."""
+
 
 class Rectangle:
+    """Represent a rectangle."""
 
     def __init__(self, width=0, height=0):
+        """Initialize a new Rectangle.
 
+        Args:
+            width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle.
+        """
         self.width = width
         self.height = height
 
     @property
     def width(self):
-
+        """Get/set the width of the Rectangle."""
         return self.__width
 
     @width.setter
@@ -22,7 +30,7 @@ class Rectangle:
 
     @property
     def height(self):
-
+        """Get/set the height of the Rectangle."""
         return self.__height
 
     @height.setter
@@ -34,29 +42,33 @@ class Rectangle:
         self.__height = value
 
     def area(self):
-
+        """Return the area of the Rectangle."""
         return (self.__width * self.__height)
 
     def perimeter(self):
-
+        """Return the perimeter of the Rectangle."""
         if self.__width == 0 or self.__height == 0:
             return (0)
         return ((self.__width * 2) + (self.__height * 2))
 
     def __str__(self):
+        """Return the printable representation of the Rectangle.
 
+        Represents the rectangle with the # character.
+        """
         if self.__width == 0 or self.__height == 0:
             return ("")
 
-        rectngle = []
+        rect = []
         for i in range(self.__height):
-            [rectngle.append('#') for j in range(self.__width)]
+            [rect.append('#') for j in range(self.__width)]
             if i != self.__height - 1:
-                rectngle.append("\n")
-        return ("".join(rectngle))
+                rect.append("\n")
+        return ("".join(rect))
 
     def __repr__(self):
+        """Return the string representation of the Rectangle."""
+        rect = "Rectangle(" + str(self.__width)
+        rect += ", " + str(self.__height) + ")"
+        return (rect)
 
-        rectngle = "Rectangle(" + str(self.__width)
-        rectngle += ", " + str(self.__height) + ")"
-        return (rectngle)

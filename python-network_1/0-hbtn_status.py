3bf489ec66ee609836bbc5eb5644e1aad5125aee
milkakeza <m.isingizwe1@alustudent.com>
2024-08-02 11:47:22 -0700

    All about REQUESTS package

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-#a Python script that fetches https://alu-intranet.hbtn.io/status using urllib
+"""a Python script that fetches https://alu-intranet.hbtn.io/status using urllib"""
 
 import urllib.request
 

867ac66b9448d44962c120fd35425dcf105c59b2
milkakeza <m.isingizwe1@alustudent.com>
2024-08-02 11:58:54 -0700

    All about REQUESTS package

--- python-network_1/10-my_github.py
+++ python-network_1/10-my_github.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-#A script that takes your GitHub credentials (username and password) and uses the GitHub API to display your id.
+"""A script that:
+takes your GitHub credentials (username and password)
+and uses the GitHub API to display your id."""
 import sys
 import requests
 from requests.auth import HTTPBasicAuth

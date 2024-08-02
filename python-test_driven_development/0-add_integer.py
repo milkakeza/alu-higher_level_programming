70c6fa22057c3d9952b211e7524d87648ebe24fc
milkakeza <m.isingizwe1@alustudent.com>
2024-08-02 12:10:29 -0700

    Let's test our py programs

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" A function for addition of an integer."""
+
+
+def add_integer(a, b=98):
+    """Returns the sum of int(a) and int(b).
+
+    Float arguments are typecasted to ints before addition is performed.
+
+    Raises:
+        TypeError: If either of a or b is a non-integer and non-float.
+    """
+    if ((not isinstance(a, int) and not isinstance(a, float))):
+        raise TypeError("a must be an integer")
+    if ((not isinstance(b, int) and not isinstance(b, float))):
+        raise TypeError("b must be an integer")
+    return (int(a) + int(b))

f3f9e79c69e353a413e0cfcb439dbabd850cd8cc
milkakeza <m.isingizwe1@alustudent.com>
2024-08-08 14:55:58 -0700

    changes

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """
 This script fetches the URL https://alu-intranet.hbtn.io/status
 using the requests package and displays the response body.
@@ -13,8 +14,7 @@ import requests
 
 if __name__ == "__main__":
     url = "https://alu-intranet.hbtn.io/status"
-    response = requests.get(url)
-    
+    response = requests.get(url) 
     print("Body response:")
     print("\t- type: {}".format(type(response.text)))
     print("\t- content: {}".format(response.text.strip()))

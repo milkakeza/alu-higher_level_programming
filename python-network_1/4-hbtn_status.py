3f5850360048aadf0656afe686d3a443cea927cd
milkakeza <m.isingizwe1@alustudent.com>
2024-08-08 14:50:15 -0700

    What's my status?

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,12 +1,20 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
-Python script that fetches https://intranet.hbtn.io/status
+This script fetches the URL https://alu-intranet.hbtn.io/status
+using the requests package and displays the response body.
+
+The response is displayed in the following format:
+    Body response:
+        - type: <class 'str'>
+        - content: OK
 """
+
 import requests
 
-if __name__ == '__main__':
-
-    res = requests.get("https://intranet.hbtn.io/status")
+if __name__ == "__main__":
+    url = "https://alu-intranet.hbtn.io/status"
+    response = requests.get(url)
+    
     print("Body response:")
-    print("\t- type: {}".format(type(res.text)))
-    print("\t- content: {}".format(res.text))
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))

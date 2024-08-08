50f92e7d233e7df28a216576039fb3985df7a79e
milkakeza <m.isingizwe1@alustudent.com>
2024-08-08 02:50:23 -0700

    Cities in state

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """
-Contains the class definition of a City
+Class definition of a City
 """
-from relationship_state import Base
+from model_state import Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 

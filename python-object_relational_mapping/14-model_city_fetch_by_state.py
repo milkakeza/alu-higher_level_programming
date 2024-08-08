deea5828cd8cec99ad08f8c199115667681e5840
milkakeza <m.isingizwe1@alustudent.com>
2024-08-08 03:36:30 -0700

    Cities in state

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 
+""" prints all City objects from the database
+
+"""
+
 from model_state import Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base

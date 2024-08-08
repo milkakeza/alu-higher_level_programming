1cc3d1bbd8513ef6d90ecb9c8d91d387cff0fb73
milkakeza <m.isingizwe1@alustudent.com>
2024-08-08 14:24:32 -0700

    corrected

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
-"""
-Python script that fetches https://intranet.hbtn.io/status
+"""Fetches url
+using the request moduel
 """
 import requests
 
-if __name__ == '__main__':
 
-    res = requests.get("https://alu-intranet.hbtn.io/status")
+if _name_ == "_main_":
+    url = 'https://intranet.hbtn.io/status'
+    if url.startswith('https://'):
+        url = "https://alu-intranet.hbtn.io/status"
+    res = requests.get(url)
     print("Body response:")
     print("\t- type: {}".format(type(res.text)))
     print("\t- content: {}".format(res.text))
